5f1d6b84ea2d3439a1b034a0eb863787e6434c43
jadianes <jadianes@gmail.com>
2015-08-20 13:58:36 +0000

    First form added

--- apps/winerama/reviews/urls.py
+++ apps/winerama/reviews/urls.py
@@ -3,8 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # ex: /review/
+    # ex: /
     url(r'^$', views.index, name='index'),
-    # ex: /reviews/5/
-    url(r'^(?P<review_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /review/5/
+    url(r'^review/(?P<review_id>[0-9]+)/$', views.review_detail, name='review_detail'),
+    # ex: /wine/5/
+    url(r'^wine/(?P<wine_id>[0-9]+)/$', views.wine_detail, name='wine_detail'),
+    url(r'^wine/(?P<wine_id>[0-9]+)/add_review/$', views.add_review, name='add_review'),
 ]
--- apps/winerama/reviews/views.py
+++ apps/winerama/reviews/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
-from .models import Review
-
+from .models import Review, Wine
+import datetime
 
 def index(request):
     latest_review_list = Review.objects.order_by('-pub_date')[:5]
@@ -9,7 +11,27 @@ def index(request):
     return render(request, 'reviews/index.html', context)
 
 
-def detail(request, review_id):
+def review_detail(request, review_id):
     review = get_object_or_404(Review, pk=review_id)
-    return render(request, 'reviews/detail.html', {'review': review})
+    return render(request, 'reviews/review_detail.html', {'review': review})
 
+
+def wine_detail(request, wine_id):
+    wine = get_object_or_404(Wine, pk=wine_id)
+    return render(request, 'reviews/wine_detail.html', {'wine': wine})
+
+
+def add_review(request, wine_id):
+    wine = get_object_or_404(Wine, pk=wine_id)
+    rating = request.POST['review_rating']
+    comment = request.POST['review_comment']
+    review = Review()
+    review.wine = wine
+    review.rating = rating
+    review.review_text = comment
+    review.pub_date = datetime.datetime.now()
+    review.save()
+    # Always return an HttpResponseRedirect after successfully dealing
+    # with POST data. This prevents data from being posted twice if a
+    # user hits the Back button.
+    return HttpResponseRedirect(reverse('reviews:wine_detail', args=(wine.id,)))
