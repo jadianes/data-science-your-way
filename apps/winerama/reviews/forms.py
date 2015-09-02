041291e452f19dd1907a26f90842c18989765328
Jose A Dianes <jadianes@gmail.com>
2015-09-02 09:53:13 +0100

    cleaning dev branch

--- apps/winerama/reviews/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from reviews.models import Review
-
-
-class ReviewForm(ModelForm):
-    class Meta:
-        model = Review
-        fields = ['user_name', 'comment', 'rating']
